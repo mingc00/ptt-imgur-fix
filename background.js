72a9168156dd33e5ff0e6774f5fe229bfbad2c0f
Mingc <ciffel@gmail.com>
2021-08-11 21:19:40 +0800

    feat: initial commit

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function removeReferer(details) {
+    for (let i = 0; i < details.requestHeaders.length; i++) {
+      const header = details.requestHeaders[i];
+      if (header.name === "Referer" && header.value === "https://www.ptt.cc/") {
+        details.requestHeaders.splice(i, 1);
+        break;
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  { urls: ["https://imgur.com/*"] },
+  ["blocking", "requestHeaders", "extraHeaders"]
+);
---
+++ pack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version=`node -e 'console.log(require("./manifest.json").version)'`
+filename="ptt-imgur.$version.zip"
+
+zip $filename background.js manifest.json
