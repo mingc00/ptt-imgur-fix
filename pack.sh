0719633ce46d59fc6e0dc3a0cb97a20cd1ecccdf
Mingc <ciffel@gmail.com>
2021-08-12 11:29:12 +0800

    fix: add icon

--- pack.sh
+++ pack.sh
@@ -3,4 +3,4 @@
 version=`node -e 'console.log(require("./manifest.json").version)'`
 filename="ptt-imgur.$version.zip"
 
-zip $filename background.js manifest.json
+zip $filename background.js manifest.json icon/48.png icon/128.png
