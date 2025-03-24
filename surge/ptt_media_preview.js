e3377f72c9ce5a6ef19252a39193170cf8d5d1d5
Mingc <ciffel@gmail.com>
2025-03-24 22:30:47 +0800

    feat: add surge module

---
+++ surge/ptt_media_preview.js
@@ -0,0 +1,12 @@
+function modifyBody(body) {
+	return body.replace('</body>', function(match) {
+		return `<script src="https://cdn.jsdelivr.net/gh/mingc00/ptt-imgur-fix@main/imgur.js"></script><script src="https://cdn.jsdelivr.net/gh/mingc00/ptt-imgur-fix@main/web.js"></script>` + match;
+	});
+}
+
+const contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
+if (contentType?.startsWith('text/html')) {
+  $done({ body: modifyBody($response.body) });
+} else {
+  $done();
+}
